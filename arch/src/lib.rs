31969331da78a5dc837c71b40b554feab320c704
QIUZHILEI <18304935041@163.com>
2024-11-05 17:21:18 +0800

    arch crate中将架构相关模块置为pub
    Signed-off-by: QIUZHILEI <18304935041@163.com>

--- arch/src/lib.rs
+++ arch/src/lib.rs
@@ -1,15 +1,8 @@
 #![no_std]
 
 #[cfg(target_arch = "riscv64")]
-mod riscv;
+pub mod riscv;
 #[cfg(target_arch = "x86_64")]
-mod x86;
+pub mod x86;
 #[cfg(target_arch = "aarch64")]
-mod aarch;
-
-#[cfg(target_arch = "riscv64")]
-pub use riscv::*;
-// #[cfg(target_arch = "aarch64")]
-// pub use aarch::*;
-// #[cfg(target_arch = "x86_64")]
-// pub use x86::*;
+pub mod aarch;
